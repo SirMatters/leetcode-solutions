301d34a9652588634a4a82a4976ae6a216795a17
Kirill Perov <perov.krll@gmail.com>
2023-08-19 16:25:31 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- my-folder/problems/group_anagrams/solution.py
+++ my-folder/problems/group_anagrams/solution.py
@@ -5,10 +5,10 @@ class Solution:
         for word in strs:
             letters = [letter for letter in word]
             sorted_letters = sorted(letters)
-            hash = ''.join(sorted_letters)
-            if hash in anagrams_map:
-                anagrams_map[hash].append(word)
+            str_hash = ''.join(sorted_letters)
+            if str_hash in anagrams_map:
+                anagrams_map[str_hash].append(word)
             else:
-                anagrams_map[hash] = [word]
+                anagrams_map[str_hash] = [word]
 
         return list(anagrams_map.values())
